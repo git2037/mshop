bb408f73cf62c790cf9738949aff57c34998c116
MinhNV <nguyenminh2708.it@gmail.com>
2026-07-15 16:55:43 +0700

    feat(user): add repository implement and base specifications

--- services/user-service/infrastructure/user-infra-data/src/main/java/com/mshop/app/user/jpa/repo/UserJPARepository.java
+++ services/user-service/infrastructure/user-infra-data/src/main/java/com/mshop/app/user/jpa/repo/UserJPARepository.java
@@ -2,10 +2,11 @@ package com.mshop.app.user.jpa.repo;
 
 import com.mshop.app.user.jpa.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface UserJPARepository extends JpaRepository<UserEntity, String> {
+public interface UserJPARepository extends JpaRepository<UserEntity, String>, JpaSpecificationExecutor<UserEntity> {
 
     Optional<UserEntity> findByEmail(String email);
 }
