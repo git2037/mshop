af3aa29dd7af5554062e5bc5dd55e3e6798a1403
MinhNV <nguyenminh2708.it@gmail.com>
2026-08-08 15:11:07 +0700

    refactor(product): add migration to drop created_by and updated_by columns

---
+++ services/product-service/product-migration/src/main/resources/changelog/source/03-drop-created_by-and-updated_by-fields.sql
@@ -0,0 +1,35 @@
+alter table attribute
+drop column created_by;
+
+alter table attribute
+drop column updated_by;
+
+alter table attribute_value
+drop column created_by;
+
+alter table attribute_value
+drop column updated_by;
+
+alter table category
+    drop column created_by;
+
+alter table category
+    drop column updated_by;
+
+alter table product
+    drop column created_by;
+
+alter table product
+    drop column updated_by;
+
+alter table product_category
+    drop column created_by;
+
+alter table product_category
+    drop column updated_by;
+
+alter table product_image
+    drop column created_by;
+
+alter table product_image
+    drop column updated_by;
